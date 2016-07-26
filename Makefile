dc90f7cf373366395fe5edaf174419b9fba76ed6
Rafael Gozlan <gozlan_r@epita.fr>
2016-07-26 20:31:46 +0200

    conf: Add configuration file install and reading

--- Makefile
+++ Makefile
@@ -2,10 +2,21 @@ GOPATH_PREFIX=github.com
 API_PKG=${GOPATH_PREFIX}/bccp/api
 MAIN_PKG=${GOPATH_PREFIX}/bccp/bccp
 
+CONFIG_DIR=/etc/bccp
+DEFAULT_CONFIG_FILE=bccp.conf
+
 GO=go
 GO_FLAGS=install
 
 all: ${API_PKG} ${MAIN_PKG}
+
+install: config
+config: ${CONFIG_DIR}
+	cp ${DEFAULT_CONFIG_FILE} ${CONFIG_DIR}
+
+${CONFIG_DIR}:
+	mkdir -p ${CONFIG_DIR}
+
 
 Makefile: ;
 .DEFAULT:
--- bccp/main.go
+++ bccp/main.go
@@ -1,12 +1,41 @@
 package main
 
 import (
+	"log"
+	"os"
 	"sync"
 
+	"github.com/BurntSushi/toml"
 	"github.com/bccp/api"
 )
 
+// Info from config file
+type Config struct {
+	server_key_file string
+	server_crt_file string
+	bccp_database   string
+	bccp_user       string
+	bccp_password   string
+}
+
+// Reads info from config file
+func ReadConfig() Config {
+	var configfile = "/etc/bccp/bccp.conf"
+	_, err := os.Stat(configfile)
+	if err != nil {
+		log.Fatal("Config file is missing: ", configfile)
+	}
+
+	var config Config
+	if _, err := toml.DecodeFile(configfile, &config); err != nil {
+		log.Fatal(err)
+	}
+	//log.Print(config.Index)
+	return config
+}
+
 func main() {
+	//var config = ReadConfig()
 	var wait sync.WaitGroup
 	api.SetupRestAPI(&wait)
 	wait.Wait()
