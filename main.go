891f880d8b2d69291223fff629f41496eab79a64
Rafael Gozlan <rafael.gozlan@epita.fr>
2016-07-28 03:41:19 +0200

    main: remove package for main

---
+++ main.go
@@ -0,0 +1,54 @@
+package main
+
+import (
+	"log"
+	"os"
+	"sync"
+
+	"github.com/BurntSushi/toml"
+	"github.com/bccp-server/api"
+	"github.com/bccp-server/mysql"
+	"github.com/bccp-server/runners"
+)
+
+// Info from config file
+type Config struct {
+	Port           string
+	Runner_port    string
+	Runner_token   string
+	Key_file       string
+	Crt_file       string
+	Mysql_database string
+	Mysql_user     string
+	Mysql_password string
+}
+
+// Reads info from config file
+func ReadConfig() Config {
+	var configfile = "/etc/bccp/bccp.conf"
+	_, err := os.Stat(configfile)
+	if err != nil {
+		log.Fatal("Config file is missing: ", configfile)
+	}
+
+	var config Config
+	if _, err := toml.DecodeFile(configfile, &config); err != nil {
+		log.Fatal(err)
+	}
+	return config
+}
+
+func main() {
+	var config = ReadConfig()
+	var wait sync.WaitGroup
+	go runners.WaitRunners(config.Runner_port, config.Runner_token)
+	api.SetupRestAPI(&wait, config.Port, config.Crt_file, config.Key_file)
+	var db mysql.Database
+	db.Connect(config.Mysql_database, config.Mysql_user, config.Mysql_password)
+	for _, r := range db.ListRunners() {
+		println("(", r.Id, " ", r.Status, ")")
+	}
+	r := db.GetRunner(1)
+	println("(", r.Id, " ", r.Status, ")")
+	wait.Wait()
+}
