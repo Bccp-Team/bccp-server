c420960c3c31f7eee1be89d4ae717d80aa98aa54
Alexis Guiho <alexis.guiho@epita.fr>
2016-10-19 11:51:01 +0200

    mysql: fix default runner for run

--- mysql/run.go
+++ mysql/run.go
@@ -152,7 +152,7 @@ func (db *Database) UpdateRunStatus(id int64, state string) error {
 }
 
 func (db *Database) AddRun(depo int64, batch int64) (int64, error) {
-	req := "INSERT INTO run VALUES(NULL,'waiting',-1,?,?,'',NULL,NULL)"
+	req := "INSERT INTO run VALUES(NULL,'waiting',0,?,?,'',NULL,NULL)"
 	insert, err := db.conn.Prepare(req)
 	if err != nil {
 		log.Print("ERROR: Unable to prepare add runner: ", err.Error())
